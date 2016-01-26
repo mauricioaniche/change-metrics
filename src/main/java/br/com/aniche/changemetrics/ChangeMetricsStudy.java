b9a8621b51e5d6e5c07849961a1189864797d444
Maurício Aniche <mauricioaniche@gmail.com>
2016-01-26 16:50:47 +0100

    supporting the change metrics in range of commits (not only on the full repository now)

--- src/main/java/br/com/aniche/changemetrics/ChangeMetricsStudy.java
+++ src/main/java/br/com/aniche/changemetrics/ChangeMetricsStudy.java
@@ -9,28 +9,41 @@ import br.com.metricminer2.Study;
 import br.com.metricminer2.persistence.csv.CSVFile;
 import br.com.metricminer2.scm.GitRepository;
 import br.com.metricminer2.scm.SCMRepository;
+import br.com.metricminer2.scm.commitrange.CommitRange;
 import br.com.metricminer2.scm.commitrange.Commits;
+import br.com.metricminer2.scm.commitrange.Range;
 
 public class ChangeMetricsStudy implements Study {
 
 	private String projectPath;
 	private String outputPath;
 	private String type;
+	private String first;
+	private String last;
 
 	public ChangeMetricsStudy(String projectPath, String outputPath, String type) {
+		this(projectPath, outputPath, type, null, null);
+	}
+
+	public ChangeMetricsStudy(String projectPath, String outputPath, String type, String first, String last) {
 		this.projectPath = projectPath;
 		this.outputPath = outputPath;
 		this.type = type;
+		this.first = first;
+		this.last = last;
 	}
 
 	public static void main(String[] args) {
 		
-		if(args == null || args.length < 3) {
-			System.out.println("Usage: java -jar <tool.jar> /dir/to/the/git/project /dir/to/the/file/output.csv all|single");
+		if(args == null || (args.length != 3 && args.length != 5)) {
+			System.out.println("Usage: java -jar <tool.jar> /dir/to/the/git/project /dir/to/the/file/output.csv all|single optional:first optional:last");
 			System.exit(-1);
 		}
 		
-		ChangeMetricsStudy study = new ChangeMetricsStudy(args[0], args[1], args[2]);
+		ChangeMetricsStudy study = args.length == 3 ?
+			new ChangeMetricsStudy(args[0], args[1], args[2]) :
+			new ChangeMetricsStudy(args[0], args[1], args[2], args[3], args[4]); 
+			
 		new MetricMiner2().start(study);
 	}
 	
@@ -44,9 +57,13 @@ public class ChangeMetricsStudy implements Study {
 				GitRepository.allProjectsIn(projectPath) : 
 				new SCMRepository[] { GitRepository.singleProject(projectPath)};
 				
+		CommitRange range = first == null ? 
+				Commits.all() : 
+				new Range(last, first);
+		
 		new RepositoryMining()
 			.in(repositories)
-			.through(Commits.all())
+			.through(range)
 			.startingFromTheBeginning()
 			.process(new ChangeMetricProcessor(repo))
 			.mine();
--- src/test/java/br/com/aniche/changemetrics/ChangeMetricsStudyTest.java
+++ src/test/java/br/com/aniche/changemetrics/ChangeMetricsStudyTest.java
@@ -27,23 +27,36 @@ public class ChangeMetricsStudyTest {
 	}
 	
 	@Test
-	public void repo1() throws IOException {
+	public void analyseTheAllRepo() throws IOException {
 		ChangeMetricsStudy study = new ChangeMetricsStudy(path, outputFile, "single");
 		study.execute();
 		
 		String result = FileUtils.readFileToString(new File(outputFile));
 		result = result.replace(path, "fulldir/");
 		
-		String correctResult = 
-				"project,file,revisions,refactorings,bugfixes,authors,locAdded,locRemoved,maxLocAdded,maxLocRemoved,avgLocAdded,avgLogRemoved,codeChurn,maxChangeset,avgChangeset,firstCommit,lastCommit,weeks\n"+
-				"repo1,fulldir/FileB.java,2,1,0,1,6,1,5,1,3.0,0.5,7,2,2.0,2015-12-09,2015-12-09,0\n"+
-				"repo1,fulldir/FileC.java,2,0,0,1,5,1,4,1,2.5,0.5,6,1,1.0,2015-12-09,2015-12-09,0\n"+
-				"repo1,fulldir/RenamedFileE.java,2,0,0,1,5,0,5,0,2.5,0.0,5,1,1.0,2015-12-09,2015-12-09,0\n"+
-				"repo1,fulldir/dir/FileD.java,1,0,0,1,4,0,4,0,4.0,0.0,4,1,1.0,2015-12-09,2015-12-09,0\n"+
-				"repo1,fulldir/FileA.java,5,1,1,1,8,2,4,1,1.6,0.4,10,2,1.4,2015-12-09,2015-12-09,0\n";
-;
-		
-		Assert.assertEquals(correctResult, result);
+		Assert.assertTrue(result.contains("project,file,revisions,refactorings,bugfixes,authors,locAdded,locRemoved,maxLocAdded,maxLocRemoved,avgLocAdded,avgLogRemoved,codeChurn,maxChangeset,avgChangeset,firstCommit,lastCommit,weeks\n"));
+		Assert.assertTrue(result.contains("repo1,fulldir/FileB.java,2,1,0,1,6,1,5,1,3.0,0.5,7,2,2.0,2015-12-09,2015-12-09,0\n"));
+		Assert.assertTrue(result.contains("repo1,fulldir/FileC.java,2,0,0,1,5,1,4,1,2.5,0.5,6,1,1.0,2015-12-09,2015-12-09,0\n"));
+		Assert.assertTrue(result.contains("repo1,fulldir/RenamedFileE.java,2,0,0,1,5,0,5,0,2.5,0.0,5,1,1.0,2015-12-09,2015-12-09,0\n"));
+		Assert.assertTrue(result.contains("repo1,fulldir/dir/FileD.java,1,0,0,1,4,0,4,0,4.0,0.0,4,1,1.0,2015-12-09,2015-12-09,0\n"));
+		Assert.assertTrue(result.contains("repo1,fulldir/FileA.java,5,1,1,1,8,2,4,1,1.6,0.4,10,2,1.4,2015-12-09,2015-12-09,0\n"));
 		
 	}
+
+	@Test
+	public void analyseByRange() throws IOException {
+		String first = "4276a1251d7ac81bca2cef7ec2adcc5af3b4768a";
+		String last = "44d6d26f20be49e129accf5e0579b1273d47c917";
+
+		ChangeMetricsStudy study = new ChangeMetricsStudy(path, outputFile, "single", first, last);
+		study.execute();
+		
+		String result = FileUtils.readFileToString(new File(outputFile));
+		result = result.replace(path, "fulldir/");
+		
+		Assert.assertTrue(result.contains("repo1,fulldir/FileA.java,5,1,1,1,8,2,4,1,1.6,0.4,10,2,1.4,2015-12-09,2015-12-09,0"));
+		Assert.assertTrue(result.contains("repo1,fulldir/FileB.java,2,1,0,1,6,1,5,1,3.0,0.5,7,2,2.0,2015-12-09,2015-12-09,0"));
+		
+	}
+	
 }
